9e1d3e732fd19ae4d8caa26c22f61b8206539f67
huyenmy239 <92309591+huyenmy239@users.noreply.github.com>
2021-10-27 19:07:43 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+var typed = new Typed(".typing1", {
+    strings: ["Biomedical Engineer", "Biochemist",  "Medical Scientist",
+    "Microbiologist"
+],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+})
